9f7f74d70309b4981ec7b7522253d37c5556da1e
YuigaWada <yuigawada@gmail.com>
2019-09-20 19:42:30 +0900

    Update Example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,36 +13,43 @@ class ViewController: PolioPagerViewController {
     
     override func viewDidLoad() {
         setup()
-        
         super.viewDidLoad()
     }
     
     private func setup()
     {
-        /*
+        
          // If you don't need a search tab, add the following code "before" super.viewDidLoad().
-         self.needSearchTab = false
-         */
+        //self.needSearchTab = false
+ 
+        //selectedBar
+        self.selectedBarHeight = 2
+        self.selectedBar.layer.cornerRadius = 0
+        self.selectedBar.backgroundColor = .gray
+        
+        //cells
+        self.eachLineSpacing = 0
     }
     
     override func tabItems()-> [TabItem] {
         
-        //I clearly have a caffeine addiction :)
-        return [TabItem(title: "Redbull"),
-                TabItem(title: "Monster"),
-                TabItem(title: "Caffeine Addiction")]
+        return [TabItem(title: "Home"),
+                TabItem(title: "Memories"),
+                TabItem(title: "Shared"),
+                TabItem(title: "Albums")]
     }
     
     override func viewControllers()-> [UIViewController]
     {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
-        let viewController1 = storyboard.instantiateViewController(withIdentifier: "searchView")
-        let viewController2 = storyboard.instantiateViewController(withIdentifier: "view1")
-        let viewController3 = storyboard.instantiateViewController(withIdentifier: "view2")
-        let viewController4 = storyboard.instantiateViewController(withIdentifier: "view3")
+        let searchViewController = storyboard.instantiateViewController(withIdentifier: "searchView")
+        let viewController1 = storyboard.instantiateViewController(withIdentifier: "view1")
+        let viewController2 = storyboard.instantiateViewController(withIdentifier: "view2")
+        let viewController3 = storyboard.instantiateViewController(withIdentifier: "view3")
+        let viewController4 = storyboard.instantiateViewController(withIdentifier: "view4")
         
-        return [viewController1, viewController2, viewController3, viewController4]
+        return [searchViewController, viewController1, viewController2, viewController3, viewController4]
     }
     
 }
